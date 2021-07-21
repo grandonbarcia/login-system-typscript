ba4ab97d44859e594d0c3e17088f3d397cb922c9
Brandon Garcia <grandonleebarcia@gmail.com>
2021-07-20 22:14:02 -0700

    added basic form for email and password

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,1 +1,22 @@
-rafce
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const Login = () => {
+    return (
+        <Form>
+            <Form.Group className="mb-3" >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" >
+                Submit
+            </Button>
+        </Form>
+    )
+}
+
+export default Login
