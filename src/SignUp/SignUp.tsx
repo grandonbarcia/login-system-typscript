df7499b416dde775fab85a3ca05c64d8e8b014f2
Brandon Garcia <grandonleebarcia@gmail.com>
2021-07-20 22:14:28 -0700

    added basic form with name, email, password inputs

--- src/SignUp/SignUp.tsx
+++ src/SignUp/SignUp.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 const SignUp = () => {
     return (
-        <div>
-            I am sign up
-        </div>
+        <Form>
+            <Form.Group className="mb-3" >
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="email" placeholder="Enter Full Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Re-Enter Password</Form.Label>
+                <Form.Control type="password" placeholder="Re-Enter Password" />
+            </Form.Group>
+            <Button variant="primary" >
+                Submit
+            </Button>
+        </Form>
     )
 }
 
