cb829c3013add943463520ffef1eeafd7ae74b2a
Brandon Garcia <grandonleebarcia@gmail.com>
2021-07-21 09:30:46 -0700

    imported useForm Hook and utilize useForm hook to save input data from Login form to state

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
+import useForm from '../Hooks/useForm'
+
 const Login = () => {
+
+
+    const [input, handleChange] = useForm({
+        email: "",
+        password: ""
+    })
+
+
+
+    useEffect(() => {
+        console.log(input);
+    }, [input])
+
+
     return (
         <Form>
             <Form.Group className="mb-3" >
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control name='email' type="email" placeholder="Enter email" onChange={handleChange} />
             </Form.Group>
             <Form.Group className="mb-3" >
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name='password' type="password" placeholder="Password" onChange={handleChange} />
             </Form.Group>
             <Button variant="primary" >
                 Submit
