80c464a8ad383dfaa27b213aaccfb6b2b1521089
Brandon Garcia <grandonleebarcia@gmail.com>
2021-07-21 09:12:30 -0700

    Created Hooks folder and added useForm hook

---
+++ src/Hooks/useForm.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+
+export const useForm = (initialState: object): [object, (e: React.ChangeEvent<HTMLInputElement>) => void] => {
+
+    const [input, setInput] = useState(initialState);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+
+    return [input, handleChange]
+
+}
+
+
+
